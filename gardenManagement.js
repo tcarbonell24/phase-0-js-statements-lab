9a46f9fbb1c830d0cb1c2de79e4a6cf408644b1f
Thomas <tcarbonell12@gmail.com>
2025-03-12 21:16:25 -0400

    completed lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,22 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off")
+};
+
+if (timeOfDay == "evening" || timeOfDay == "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off")
+};
+
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    if (soilMoisture == 40) {
+        console.log(soilMoisture);
+    }
+};
